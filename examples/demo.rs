6868c0262f0197c9e7785d0311a9f6dffd0e0cf4
Sanpi <sanpi@homecomputing.fr>
2018-05-22 19:08:03 +0200

    Adds demo

---
+++ examples/demo.rs
@@ -0,0 +1,33 @@
+extern crate lxc;
+
+fn main() {
+    let c = ::lxc::Container::new("apicontainer", None)
+        .expect("Failed to setup lxc_container struct");
+
+    if c.is_defined() {
+        panic!("Container already exists");
+    }
+
+    c.create(
+        "download",
+        None,
+        None,
+        ::lxc::CreateFlags::QUIET,
+        &["-d", "ubuntu", "-r", "trusty", "-a", "i386"],
+    ).expect("Failed to create container rootfs");
+
+    c.start(false, &[])
+        .expect("Failed to start the container");
+
+    println!("Container state: {}", c.state());
+    println!("Container PID: {}", c.init_pid());
+
+    if c.shutdown(30).is_err() {
+        println!("Failed to cleanly shutdown the container, forcing.");
+        c.stop()
+            .expect("Failed to kill the container.");
+    }
+
+    c.destroy()
+        .expect("Failed to destroy the container.");
+}
