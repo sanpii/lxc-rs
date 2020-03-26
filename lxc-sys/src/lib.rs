d765d088ff37346daa18f5d948193c0e46b62ded
Sanpi <sanpi@homecomputing.fr>
2020-03-26 11:53:03 +0100

    [gitlab-ci] Adds clippy check

--- lxc-sys/src/lib.rs
+++ lxc-sys/src/lib.rs
@@ -1,5 +1,8 @@
+#![allow(improper_ctypes)]
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(clippy::redundant_static_lifetimes)]
+#![allow(clippy::unreadable_literal)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
