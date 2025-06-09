a6bd233775beb59762faf67da4cdf8f44da255dc
Sanpi <sanpi@homecomputing.fr>
2025-06-09 12:44:36 +0200

    Updates clippy lint allow list

--- lxc-sys/src/lib.rs
+++ lxc-sys/src/lib.rs
@@ -3,6 +3,8 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(clippy::identity_op)]
+#![allow(clippy::missing_safety_doc)]
+#![allow(clippy::ptr_offset_with_cast)]
 #![allow(clippy::unnecessary_operation)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
